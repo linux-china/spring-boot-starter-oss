37ce02c0e7560884fc8fdaebcf5173384bf471e3
linux_china <linux_china@hotmail.com>
2015-11-10 21:16:27 +0800

    add test methods

--- src/test/java/org/mvnsearch/boot/AliyunOssAutoConfigurationTest.java
+++ src/test/java/org/mvnsearch/boot/AliyunOssAutoConfigurationTest.java
@@ -1,15 +1,21 @@
 package org.mvnsearch.boot;
 
 import com.aliyun.oss.OSSClient;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mvnsearch.boot.oss.AliyunOssAutoConfiguration;
 import org.mvnsearch.boot.oss.AliyunOssProperties;
+import org.mvnsearch.boot.oss.ByteArrayDataSource;
+import org.mvnsearch.boot.oss.FileStorageService;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.activation.DataSource;
 
 /**
  * aliyun oss auto configuration test
@@ -19,6 +25,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AliyunOssAutoConfigurationTest {
     private AnnotationConfigApplicationContext context;
+
+    @Before
+    public void setUp() {
+        this.context = new AnnotationConfigApplicationContext(
+                AliyunOssAutoConfigurationTest.class, AliyunOssAutoConfiguration.class);
+    }
 
     @After
     public void close() {
@@ -31,15 +43,27 @@ public class AliyunOssAutoConfigurationTest {
     public AliyunOssProperties properties() {
         AliyunOssProperties properties = new AliyunOssProperties();
         properties.setKey("xxx");
-        properties.setSecret("xxx");
+        properties.setSecret("xxxx");
+        properties.setBucket("xxx-dev");
         return properties;
     }
 
     @Test
     public void clientExists() {
-        this.context = new AnnotationConfigApplicationContext(
-                AliyunOssAutoConfigurationTest.class, AliyunOssAutoConfiguration.class);
         Assert.assertEquals(1, this.context.getBeanNamesForType(OSSClient.class).length);
+        Assert.assertEquals(1, this.context.getBeanNamesForType(FileStorageService.class).length);
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        byte[] content = IOUtils.toByteArray(this.getClass().getResourceAsStream("/1.jpg"));
+        ByteArrayDataSource bads = new ByteArrayDataSource(content, "image/jpeg");
+        bads.setName("1.jpg");
+        FileStorageService fileStorageService = context.getBean(FileStorageService.class);
+        String newFileName = fileStorageService.save(bads);
+        DataSource fileDS = fileStorageService.get(newFileName);
+        Assert.assertNotNull(fileDS);
+        fileStorageService.delete(newFileName);
     }
 
 }
