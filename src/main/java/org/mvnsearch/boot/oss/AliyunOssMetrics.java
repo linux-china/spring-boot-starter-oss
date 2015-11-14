d9fe3b788d2a9a2e4be5dc372b89259afd15919c
linux_china <linux_china@hotmail.com>
2015-11-14 20:30:48 +0800

    aliyun OSS metrics

---
+++ src/main/java/org/mvnsearch/boot/oss/AliyunOssMetrics.java
@@ -0,0 +1,30 @@
+package org.mvnsearch.boot.oss;
+
+import org.mvnsearch.boot.oss.impl.FileStorageServiceOssImpl;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.actuate.metrics.Metric;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * aliyun OSS metrics
+ *
+ * @author leijuan
+ */
+@Component
+public class AliyunOssMetrics implements PublicMetrics {
+
+    public Collection<Metric<?>> metrics() {
+        List<Metric<?>> metrics = new LinkedList<Metric<?>>();
+        metrics.add(new Metric<Number>("oss.upload.success", FileStorageServiceOssImpl.fileUploadSuccess));
+        metrics.add(new Metric<Number>("oss.upload.fail", FileStorageServiceOssImpl.fileUploadFail));
+        metrics.add(new Metric<Number>("oss.get.count", FileStorageServiceOssImpl.fileGetCounts));
+        metrics.add(new Metric<Number>("oss.delete.count", FileStorageServiceOssImpl.fileDeleteCounts));
+        return metrics;
+    }
+
+
+}
