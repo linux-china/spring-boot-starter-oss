af0caeb78592c6b434d0c6c26075e0c4a0b1629e
linux_china <linux_china@hotmail.com>
2015-11-23 20:33:06 +0800

    oss test application

---
+++ src/test/java/org/mvnsearch/boot/OssTestApplication.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * oss test application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+@PropertySource("/application.properties")
+public class OssTestApplication {
+
+}
